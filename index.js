76b20aa4df7a62fde1e91445004104e7b3f03c72
desmond beramendi <desmond7187@gmail.com>
2018-01-03 22:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,14 @@ describe('sayHiToGrandma(string)', function() {
   })
 })
 */
-
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "I love you Grandma.") {
+    return "I love you, too";
+  } else {
+    return "Speak up sonny!";
+  }
+}
