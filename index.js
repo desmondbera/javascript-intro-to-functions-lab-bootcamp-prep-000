cd69965ddea07fd31f952f9d6e7f2b9221db9d25
desmond beramendi <desmond7187@gmail.com>
2018-01-03 22:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+
+/*
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
+*/
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
